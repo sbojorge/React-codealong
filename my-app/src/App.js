33eb7ffd1e3b4b64653fc37912fcb72efa67ebcc
sbojorge <>
2023-05-08 15:45:58 +0000

    create a stateful component with props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+import StatefulGreeting from "./components/StatefulGreeting";
 
 function App() {
     return (
         <div className="App">
-            
-            <FunctionalGreetingWithProps greeting="nice to meet you!" name="Gatita" age="41"/>
+
+            <StatefulGreeting greeting="I'm a stateful component" name="Gatita" />
         </div>
     );
 }
