d77e224a57d76e0eb6bd87feff230a901f93012b
sbojorge <>
2023-05-09 08:44:08 +0000

    create ES7 snippet

---
+++ my-app/src/components/ES7snippets.js
@@ -0,0 +1,49 @@
+// Create a functional component and export it
+// import React from 'react'
+
+// function ES7snippets() {
+//   return (
+//     <div>ES7snippets</div>
+//   )
+// }
+
+// export default ES7snippets
+
+// Create and export a component as an arrow function
+// import React from 'react'
+
+// const ES7snippets = () => {
+//   return (
+//     <div>ES7snippets</div>
+//   )
+// }
+
+// export default ES7snippets
+
+// Create and export a class component
+// import React, { Component } from 'react'
+
+// export class ES7snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7snippets
+
+// Create a constructor
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// Log something to the console
+// console.log(first)
+
+// log an object  along with its name as a string.
+// console.log('first', first)
