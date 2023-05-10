4b705afe8a34a11e4eb752013ad5d35e416db411
sbojorge <>
2023-05-10 11:14:20 +0000

    create nested components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+import NestingComponents from './components/NestingComponents';
 
 function App() {
     return (
         <div className="App">
-            <ConditionalRenderingFunctional connected={false} />
-            <ConditionalRenderingClass />
+            <NestingComponents />
+
 
         </div>
 
